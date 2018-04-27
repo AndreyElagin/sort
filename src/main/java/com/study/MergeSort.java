a70a6cf83f11c2008f993072544f1974f22424ac
Andrei Elagin <andrei_elagin@epam.com>
2018-04-27 17:03:29 +0300

    MergeSort implemented

--- src/main/java/com/study/MergeSort.java
+++ src/main/java/com/study/MergeSort.java
@@ -2,17 +2,15 @@ package com.study;
 
 public final class MergeSort {
 
-    private static int[] aux;
-
     private MergeSort() {
     }
 
     public static void sort(int[] array) {
-        aux = new int[array.length];
-        sort(array, 0, array.length - 1);
+        int[] aux = new int[array.length];
+        sort(aux, array, 0, array.length - 1);
     }
 
-    private static void merge(int array[], int low, int mid, int high) {
+    private static void merge(int aux[], int array[], int low, int mid, int high) {
         int i = low;
         int j = mid + 1;
 
@@ -31,14 +29,14 @@ public final class MergeSort {
         }
     }
 
-    private static void sort(int[] array, int low, int high) {
+    private static void sort(int aux[], int[] array, int low, int high) {
         if (high <= low) {
             return;
         }
         int mid = low + (high - low) / 2;
-        sort(array, low, mid);
-        sort(array, mid + 1, high);
-        merge(array, low, mid, high);
+        sort(aux, array, low, mid);
+        sort(aux, array, mid + 1, high);
+        merge(aux, array, low, mid, high);
     }
 
 }
