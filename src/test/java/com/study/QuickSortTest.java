c0eaec0831c8345281aa082aebd5dab915c2c883
Andrei Elagin <andrei_elagin@epam.com>
2018-04-27 18:31:57 +0300

    QuickSort implemented

---
+++ src/test/java/com/study/QuickSortTest.java
@@ -0,0 +1,24 @@
+package com.study;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class QuickSortTest {
+
+    private int[] testArray;
+    private int[] sortedArray;
+
+    @Before
+    public void init() {
+        testArray = new int[]{2, 3, 5, 1, 4, 6};
+        sortedArray = new int[]{1, 2, 3, 4, 5, 6};
+    }
+
+    @Test
+    public void sortTest() {
+        QuickSort.sort(testArray);
+        assertArrayEquals(sortedArray, testArray);
+    }
+}
